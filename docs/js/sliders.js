fdc535acd1e1b1a5d1b68f0c5b7437855ad71d6f
Skaper777 <skaper777@gmail.com>
2019-06-02 19:34:28 +0300

    blog-1

--- docs/js/sliders.js
+++ docs/js/sliders.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var mainSlider = new Swiper('.main-slider', {
+  let mainSlider = new Swiper('.main-slider', {
     slidesPerView: 1,
     allowTouchMove: false,
     followFinger: false,
@@ -11,5 +11,33 @@
     autoplay: {
       delay: 3000
     }
-  })
+  });
+
+  let buttonLeft = document.querySelector('.blog-controls__arrows-left');
+  let buttonRight = document.querySelector('.blog-controls__arrows-right');
+
+  let slides = document.querySelectorAll('blog-slider__slide');
+
+  let slidesArr = [];
+  let currentSlide = 0;
+
+  for (let i = 0; i < slides.length; i++) {
+    slidesArr[i] = slides[i];  
+  }
+
+  buttonRight.addEventListener('click', function () {
+    if (currentSlide < slidesArr.length - 1) {     
+      slides[currentSlide].classList.remove('showing');
+      currentSlide += 1;
+      slides[currentSlide].classList.add('showing');
+    }  
+  });
+
+  buttonLeft.addEventListener('click', function () {  
+    if (currentSlide > 0) {    
+      slidesArr[currentSlide].classList.remove('showing');
+      currentSlide -= 1;
+      slidesArr[currentSlide].classList.add('showing');
+    }
+  });
 })();

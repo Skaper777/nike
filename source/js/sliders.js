1e75c14363b2337dabe7ea7f948af2056adc9425
Skaper777 <skaper777@gmail.com>
2019-06-03 17:30:01 +0300

    blog-2

--- source/js/sliders.js
+++ source/js/sliders.js
@@ -16,28 +16,45 @@
   let buttonLeft = document.querySelector('.blog-controls__arrows-left');
   let buttonRight = document.querySelector('.blog-controls__arrows-right');
 
-  let slides = document.querySelectorAll('blog-slider__slide');
+  let slides = document.querySelectorAll('.blog-slider__slide');
+
+  let currentNum = document.querySelector('.blog-controls__pagination-current');
+  let totalNum = document.querySelector('.blog-controls__pagination-total');
 
   let slidesArr = [];
   let currentSlide = 0;
+  
+  totalNum.textContent = '0' + slides.length;
 
   for (let i = 0; i < slides.length; i++) {
     slidesArr[i] = slides[i];  
-  }
+  }  
 
   buttonRight.addEventListener('click', function () {
     if (currentSlide < slidesArr.length - 1) {     
       slides[currentSlide].classList.remove('showing');
+      currentNum.textContent = '0' + ((currentSlide + 1) + 1);      
       currentSlide += 1;
       slides[currentSlide].classList.add('showing');
     }  
+
+    if (currentSlide === (slides.length - 1)) {
+      buttonRight.classList.add('disabled');
+      buttonLeft.classList.remove('disabled');
+    } else if (currentSlide > 0) {
+      buttonLeft.classList.remove('disabled');
+    }
   });
 
   buttonLeft.addEventListener('click', function () {  
-    if (currentSlide > 0) {    
+    if (currentSlide > 0) {          
       slidesArr[currentSlide].classList.remove('showing');
-      currentSlide -= 1;
+      currentNum.textContent = '0' + ((currentSlide + 1) - 1);
+      currentSlide -= 1;      
       slidesArr[currentSlide].classList.add('showing');
-    }
+      buttonRight.classList.remove('disabled');     
+    } else if (currentSlide === 0) {
+      buttonLeft.classList.add('disabled');
+    }    
   });
 })();
